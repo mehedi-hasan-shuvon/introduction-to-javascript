94eb9507937f13203c156ccf0e6a4d20381aa4f4
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-01-26 01:25:57 +0600

    practising js

--- first.js
+++ first.js
@@ -1,3 +1,31 @@
-var name= "Hero Alan";
+var a = 7;
+var b = 6;
+var total = b - a;
+a = a + b;
+a += 10;
+a++;
+var c = "dfd"
+var d = " fdsf"
+var e = c + d
+var j = true;
 
-console.log(name);
+
+// convert string to integer 
+var f = "3fhj45";
+var g = parseInt(f);
+var k = parseFloat(f)
+g++;
+k++;
+var h = null;
+var p = new Date()
+console.log(p);
+
+//speical editon
+var a = 0.1;
+var b = 0.2;
+var total = a + b;
+console.log(total);
+total=total.toFixed(1);
+total=parseFloat(total);
+console.log(total)
+// console.log(total.toFixed(1));
