62958d65c34617ed9c77c862782a6962d2cb639d
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-01-31 23:38:48 +0600

    practising math

--- math.js
+++ math.js
@@ -6,3 +6,12 @@
 // const output = Math.ceil(myNumber);
 // const output = Math.floor(myNumber);
 // console.log(output);
+
+
+// 0 to 1 random number
+// const output = Math.random(); 
+
+// 0 to any random number;
+const output=Math.random()*10;
+const rounded=Math.round(output);
+console.log(rounded);
