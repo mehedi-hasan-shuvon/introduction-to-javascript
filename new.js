67c53ba2070daead9fba191879eb53226e7cc681
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-01-31 22:57:21 +0600

    fsddf

--- new.js
+++ new.js
@@ -33,18 +33,65 @@ function substraction(a, b) {
 
 // objects 
 
-var student1={
-    id:18101686,
-    name:"Mehedi", 
-    cgpa:3.88, 
-    phoneNumber:"01731876347",
+var student1 = {
+    id: 18101686,
+    name: "Mehedi",
+    cgpa: 3.88,
+    phoneNumber: "01731876347",
 };
 
-var mobile={
-    color:"black",
+var mobile = {
+    color: "black",
     price: 1900,
     screenSize: 6,
     storage: '16gb',
 };
 
 console.log(student1.cgpa);
+
+// get a object property 
+cgpa = student1.cgpa
+console.log(cgpa);
+
+// set a property 
+student1.name = "tehedi"
+
+// another way 
+studentName = "name";
+student1[studentName] = 'popy';
+console.log(student1);
+// another way to set a value 
+student1["name"] = 'jehedi'
+console.log(student1);
+
+
+
+
+
+// switch 
+
+var color = 'red';
+
+switch (color) {
+    case 'blue':
+        console.log('color is blue');
+        break;
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'green':
+        console.log('color is green');
+        break;
+    default:
+        console.log('color is black');
+
+}
+
+
+function getReminder(number1, number2) {
+    number1 % number2;
+}
+
+const reminder = getReminder(12, 2);
+
+console.log(reminder);
