1e0281b84fc20769548e9b9f323e43d14b5cf198
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-01-31 23:35:22 +0600

    practising math

--- math.js
+++ math.js
@@ -1,8 +1,8 @@
 // const muNumber = -5;
 // const output = Math.abs(muNumber);
 
-const myNumber = 2.5398;
-
-const output = Math.round(myNumber);
-
-console.log(output);
+// const myNumber = 2.22398;
+// const output = Math.round(myNumber);
+// const output = Math.ceil(myNumber);
+// const output = Math.floor(myNumber);
+// console.log(output);
