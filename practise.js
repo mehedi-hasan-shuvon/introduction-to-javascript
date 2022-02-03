f932eeddd1cd9f9e604d3ff27c26885b1694e620
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-02-03 22:05:33 +0600

    pratise more js

--- practise.js
+++ practise.js
@@ -19,18 +19,25 @@ console.log(vowelCounter(mystring));
 
 //problem 2
 
-let quantity = 3;
+let quantity = -3;
 
 function price(quantity) {
     let price = 0;
     const first2kg = 30;
     const restkg = 25;
+    if (quantity < 0) {
+        return "quantity cann't be negetive";
+        
+    }
+    if (typeof quantity != "number") {
+        return "please enter a valid number";
+    }
     if (quantity <= 2) {
         price = quantity * first2kg;
     } else {
-        price = (2) * first2kg + (quantity - 2) * restkg;
+        price = 2 * first2kg + (quantity - 2) * restkg;
     }
     return price;
 }
 
-console.log(price(quantity));
+console.log(price(-5));
