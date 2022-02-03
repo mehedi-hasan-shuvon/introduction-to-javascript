68c0a1d198071439011ad394fcba16cc59b13aa9
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-02-03 21:54:52 +0600

    pratise more js

--- practise.js
+++ practise.js
@@ -1,9 +1,9 @@
 // count the number of vowelts in a string
-
+/*
 const mystring = "Quick brown fox jumps over the lazy dog"
 
 function vowelCounter(mystring) {
-    count = 0;
+    let count = 0;
     const vowel = "aeiouAEIOU";
     for (let char of mystring) {
         if (vowel.indexOf(char) != -1) {
@@ -14,3 +14,23 @@ function vowelCounter(mystring) {
 }
 
 console.log(vowelCounter(mystring));
+
+*/
+
+//problem 2
+
+let quantity = 3;
+
+function price(quantity) {
+    let price = 0;
+    const first2kg = 30;
+    const restkg = 25;
+    if (quantity <= 2) {
+        price = quantity * first2kg;
+    } else {
+        price = (2) * first2kg + (quantity - 2) * restkg;
+    }
+    return price;
+}
+
+console.log(price(quantity));
