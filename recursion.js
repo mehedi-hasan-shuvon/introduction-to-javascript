8120d0d2115baff9ea9fdb37e26d9465b8026f90
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-02-05 22:19:44 +0600

    practising sum

--- recursion.js
+++ recursion.js
@@ -1,1 +1,25 @@
-console.log("hello world");
+// for (let i = 1; i < 6; i++) {
+//     console.log(i);
+// }
+
+/*
+function consoleNumber(i) {
+    if (i >5) {
+        return;
+    }
+    console.log(i);
+    consoleNumber(i + 1);
+}
+
+consoleNumber(1);
+
+*/
+
+function sum(i) {
+    if (i == 0) {
+        return 0;
+    }
+    return i + sum(i - 1);
+}
+
+console.log(sum(5));
